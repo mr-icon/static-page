d92e25815cc6f429d22c27be5dc0352dc4cdb582
mr-icon <emmanuelaustin116@gmail.com>
2022-12-22 01:26:32 -0800

    pushing back dark-mode

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,6 +2,10 @@ import React from "react";
 import reactLogo from "../images/logo.svg"
 
 function Header() {
+  function handleChange (){
+    document.body.classList.toggle("dark-theme");
+  }
+
   return (
     <header>
       <nav className="nav">
@@ -11,6 +15,10 @@ function Header() {
           <li>Fun</li>
           <li>Facts</li>
         </ul>
+        <label class="switch">
+          <input type="checkbox" onChange={handleChange}/>
+          <span class="slider"></span>
+        </label>
       </nav>
     </header>
   );
